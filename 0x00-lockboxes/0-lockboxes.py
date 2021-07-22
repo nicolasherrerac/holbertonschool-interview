2e1b2cdea83024cb21c295e0e49cee2b7346534c
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-07-22 17:30:23 -0500

    Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+canUnlockAll method that determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)
+    keys = [0]
+    for key in keys:
+        key_in_box = boxes[key]
+        for key in key_in_box:
+            if key not in keys and key < num_boxes:
+                keys.append(key)
+    else:
+        if len(keys) < num_boxes:
+            return False
+        return True
