b1f4d8444f8092fe4f06afd49586172b8a8e5483
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-08-05 18:54:21 -0500

    Fix

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,24 +1,76 @@
-#include"binary_trees.h"
+#include "binary_trees.h"
+
 /**
- * heap_insert - function that inserts a value into a Max Binary Heap
- * @root: is a double pointer to the root node of the Heap
- * @value: is the value store in the node to be inserted
+ * heap_insert - Inserts a value into a Max Binary Heap
  *
- * Return: a pointer to the inserted node, or NULL on failure
- **/
+ * @root: Double pointer to the root node of the Heap
+ * 
+ * @value: The value store in the node to be inserted
+ * 
+ * Return: A tree with max heap ordering
+ */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node = NULL;
+	heap_t *node, *parent;
 
-	new_node = malloc(sizeof(*new_node));
+	/* For an empty tree, create nodes and return tree pointer to tree */
+	if (*root == NULL)
+	{
+		node = binary_tree_node(*root, value);
+		*root = node;
+		return (*root);
+	}
+	node = *root;
 
-	if (!new_node)
-		return (NULL);
+	/* Traverse tree */
+	while (node != NULL)
+	{
+		parent = node;
+		/* Ignore duplicates */
+		if (node->n == value)
+			return node_swap(node);
 
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = *root;
+		/* Sort tree into BST formation */
+		if (value > node->n)
+			node = parent->left;
+		else
+			node = node->right;
+	}
 
-	return (new_node);
+	/* Sort parent nodes */
+	if (value > parent->n)
+	{
+		parent->left = binary_tree_node(parent, value);
+		return (parent->left);
+	}
+	else
+	{
+		parent->right = binary_tree_node(parent, value);
+		return (parent->right);
+	}
+}
+
+
+/**
+ * node_swap - Swaps current node with larger node
+ *
+ * @node: Pointer to current node
+ * 
+ * Return: A new node
+ */
+
+heap_t *node_swap(heap_t *node)
+{
+	while (node && node->parent)
+	{
+		while (node->n > node->parent->n)
+		{
+			node = node->parent;
+			node->n += node->parent->n;
+			node->parent->n -= node->n;
+			node->parent->n = node->n - node->parent->n;
+		}
+	}
+	return (node);
 }
